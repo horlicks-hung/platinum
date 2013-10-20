a662fcb785c473d2330d1dbaa8b8e13f39d35b0f
horlicks.hung@chaogun.com <horlicks.hung@chaogun.com>
2013-10-21 02:09:22 +0800

    add dao and beans

---
+++ PlatinumDao/src/main/java/com/platinumwill/dao/hibernate/AbstractDao.java
@@ -0,0 +1,19 @@
+package com.platinumwill.dao.hibernate;
+
+import org.hibernate.SessionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public abstract class AbstractDao {
+	private SessionFactory sessionFactory;
+	protected Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	public SessionFactory getSessionFactory() {
+		return this.sessionFactory;
+	}
+	@Autowired
+	public void setSessionFactory(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+}
