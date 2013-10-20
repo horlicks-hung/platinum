ce93eca631d205276aac3abdfbf9d4a79a17a221
horlicks.hung@chaogun.com <horlicks.hung@chaogun.com>
2013-10-21 03:39:19 +0800

    add parent and children properties to TreeItem

--- PlatinumBeans/src/main/java/com/platinumwill/entity/jpa/TreeItem.java
+++ PlatinumBeans/src/main/java/com/platinumwill/entity/jpa/TreeItem.java
@@ -1,10 +1,15 @@
 package com.platinumwill.entity.jpa;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -23,5 +28,23 @@ public class TreeItem {
 	}
 	
 	
+	private TreeItem parent;
+	@ManyToOne()
+	@JoinColumn(name = "PARENT_ID", nullable = true, insertable = false, updatable = false)
+	public TreeItem getParent() {
+		return this.parent;
+	}
+	public void setParent(TreeItem parent) {
+		this.parent = parent;
+	}
+	
+	private List<TreeItem> children;
+	@OneToMany(mappedBy = "parent")
+	public List<TreeItem> getChildren() {
+		return this.children;
+	}
+	public void setChildren(List<TreeItem> children) {
+		this.children = children;
+	}
 	
 }
