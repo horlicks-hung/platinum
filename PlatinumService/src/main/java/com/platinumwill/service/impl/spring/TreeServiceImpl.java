57ee48ce185d43a6504d2fac60fdd70d5ce8432d
horlicks.hung@chaogun.com <horlicks.hung@chaogun.com>
2013-10-21 04:08:51 +0800

    change the package of TreeServiceImpl

--- PlatinumService/src/main/java/com/platinumwill/service/impl/spring/TreeServiceImpl.java
+++ PlatinumService/src/main/java/com/platinumwill/service/impl/spring/TreeServiceImpl.java
@@ -1,14 +1,26 @@
-package com.platinumwill.service.impl;
+package com.platinumwill.service.impl.spring;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.platinumwill.dao.TreeDao;
+import com.platinumwill.entity.jpa.TreeItem;
 import com.platinumwill.service.TreeService;
 
 @Component
 public class TreeServiceImpl implements TreeService {
 
+	public void mockService() {
+		TreeItem dad = new TreeItem("dad");
+		TreeItem son = new TreeItem("son");
+		TreeItem daughter = new TreeItem("daughter");
+		son.setParent(dad);
+		daughter.setParent(dad);
+		this.treeDao.persist(dad);
+		this.treeDao.persist(daughter);
+		this.treeDao.persist(son);
+	}
+	
 	private TreeDao treeDao;
 	public TreeDao getTreeDao() {
 		return this.treeDao;
