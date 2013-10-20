39c9887df79d754a5f76208f7721b07b968c7b11
horlicks.hung@chaogun.com <horlicks.hung@chaogun.com>
2013-10-21 02:23:07 +0800

    add properties of service

--- PlatinumTest/src/test/java/com/platinumwill/test/TempTest.java
+++ PlatinumTest/src/test/java/com/platinumwill/test/TempTest.java
@@ -9,8 +9,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.platinumwill.service.TreeService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "/test-beans.xml", "/beans.xml" })
@@ -19,9 +22,19 @@ public class TempTest {
 	public static Logger getLogger() {
 		return LOGGER;
 	}
+	
+	private TreeService treeService;
+	public TreeService getTreeService() {
+		return this.treeService;
+	}
+	@Autowired
+	public void setTreeService(TreeService treeService) {
+		this.treeService = treeService;
+	}
 
 	@Test
 	public void test() {
+		LOGGER.debug("test message");
 	}
 	
 }
