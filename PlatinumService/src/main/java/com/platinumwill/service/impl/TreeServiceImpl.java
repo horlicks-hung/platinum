2ece10c148e133b83455a1737d17f371b6b62b6a
horlicks.hung@chaogun.com <horlicks.hung@chaogun.com>
2013-10-21 02:53:17 +0800

    add @Component to TreeServiceImpl.java

--- PlatinumService/src/main/java/com/platinumwill/service/impl/TreeServiceImpl.java
+++ PlatinumService/src/main/java/com/platinumwill/service/impl/TreeServiceImpl.java
@@ -1,10 +1,12 @@
 package com.platinumwill.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import com.platinumwill.dao.TreeDao;
 import com.platinumwill.service.TreeService;
 
+@Component
 public class TreeServiceImpl implements TreeService {
 
 	private TreeDao treeDao;
