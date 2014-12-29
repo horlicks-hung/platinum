644070984a3060447c5f5ad90ca81ad254fd0324
horlicks.hung@chaogun.com <horlicks.hung@chaogun.com>
2014-12-29 15:29:04 +0800

    新增 isPresent() 和 DEFAULT_TIMEOUT_IN_SECONDS。

--- SeleniumAgent/src/main/java/com/platinum/test/selenium/SeleniumVerifyAgent.java
+++ SeleniumAgent/src/main/java/com/platinum/test/selenium/SeleniumVerifyAgent.java
@@ -1,6 +1,7 @@
 package com.platinum.test.selenium;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedCondition;
@@ -8,6 +9,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class SeleniumVerifyAgent {
+	private static final int DEFAULT_TIMEOUT_IN_SECONDS = 5;
 	private WebDriver driver;
 	
 	public SeleniumVerifyAgent(WebDriver driver) {
@@ -16,7 +18,7 @@ public class SeleniumVerifyAgent {
 
 	public boolean isElementEnabled(By 怎麼找到element) {
 		ExpectedCondition<WebElement> 選項看得到 = ExpectedConditions.presenceOfElementLocated(怎麼找到element);
-		WebElement element = new WebDriverWait(driver, 1).until(選項看得到);
+		WebElement element = new WebDriverWait(driver, DEFAULT_TIMEOUT_IN_SECONDS).until(選項看得到);
 		if (element.isEnabled()) {
 			return true;
 		}
@@ -25,8 +27,18 @@ public class SeleniumVerifyAgent {
 
 	public boolean isElementDisabled(By 怎麼找到element) {
 		ExpectedCondition<WebElement> 選項看得到 = ExpectedConditions.presenceOfElementLocated(怎麼找到element);
-		WebElement element = new WebDriverWait(driver, 1).until(選項看得到);
+		WebElement element = new WebDriverWait(driver, DEFAULT_TIMEOUT_IN_SECONDS).until(選項看得到);
 		if (element.isEnabled()) {
+			return false;
+		}
+		return true;
+	}
+	
+	public boolean isPresent(By by) {
+		ExpectedCondition<WebElement> elementIsPresent = ExpectedConditions.presenceOfElementLocated(by);
+		try {
+			new WebDriverWait(driver, DEFAULT_TIMEOUT_IN_SECONDS).until(elementIsPresent);
+		} catch (TimeoutException e) {
 			return false;
 		}
 		return true;
